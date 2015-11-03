84eb55d42d7bd42f46244f0696f17dfdec3ecb4f
Marc Doan <marc@bentonow.com>
2015-11-03 15:34:37 -0800

    Do not use node load balancers

--- config/shared/dev.js
+++ config/shared/dev.js
@@ -1,4 +1,4 @@
 // Environment-specific configurations symlink'ed by Capistrano to js/config.js
 // during deployment
-var NODE_URL = 'http://node.dev.bentonow.com';
+var NODE_URL = 'http://54.191.141.101:8081';
 var HOUSTON_URL = 'http://houston.dev.bentonow.com';
