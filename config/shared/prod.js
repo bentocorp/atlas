fa9dacaa348973e4a76b2908f2f3c4146fd40537
Marc Doan <marc@bentonow.com>
2015-11-03 15:51:56 -0800

    Forgot to add http:// in houston prod url

--- config/shared/prod.js
+++ config/shared/prod.js
@@ -1,4 +1,4 @@
 // Environment-specific configurations symlink'ed by Capistrano to js/config.js
 // during deployment
 var NODE_URL = 'http://52.32.68.149:8081';
-var HOUSTON_URL = 'houston.bentonow.com';
+var HOUSTON_URL = 'http://houston.bentonow.com';
