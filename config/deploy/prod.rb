91149bb9bd0fe469c07e28814af425cdfdee3a8f
Marc Doan <marc@bentonow.com>
2015-11-30 17:25:13 -0800

    Added bento-prod-node02 to Capistrano deploy file

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -17,7 +17,7 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{bento-prod-node01}
+role :app, %w{bento-prod-node01 bento-prod-node02}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
