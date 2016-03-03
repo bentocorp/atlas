7b5f0b210e21a61a1ff2a9e146134e08c621e6bf
Vincent Cardillo <vcardillo@gmail.com>
2016-03-03 12:37:40 -0800

    Local file.

--- config/shared/local.js
+++ config/shared/local.js
@@ -1,5 +1,10 @@
 // Environment-specific configurations symlink'ed by Capistrano to js/config.js
 // during deployment
-var NODE_URL = 'http://127.0.0.1:8085';
+//
+var NODE_URL = 'http://127.0.0.1:8080'; //VJC
 //var NODE_URL = 'https://node.dev.bentonow.com:8443';
-var HOUSTON_URL = 'http://127.0.0.1:9001';
+var HOUSTON_URL = 'http://127.0.0.1:9000'; //VJC
+
+
+//var NODE_URL = 'http://10.20.1.75:8080'; //VJC
+//var HOUSTON_URL = 'http://10.20.1.75:9000'; //VJC
